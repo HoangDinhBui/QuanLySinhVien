877c1e390dbfcaeb8a033eead005650331db2381
Hoang-Mr5 <buidinhhoang1910@gmail.com>
2023-12-15 16:53:39 +0700

    Fix addStudent function

--- quanLySinhVien.cpp
+++ quanLySinhVien.cpp
@@ -1,33 +1,36 @@
 #include <stdio.h>
-#include <windows.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 typedef struct Students{
     char name[30];
     long long id;
     char dob[13];
     float gpa;
 }ST;
-void addStudents(ST *stArr, int *numOfStudent)
+void addStudent(ST *stArr, int *numOfStudent)
 {
+    (*numOfStudent)++;
+    stArr = (ST*)realloc(stArr, *numOfStudent * sizeof(ST));
     ST student;
     printf("\nNhap ten sinh vien: ");
     while(getchar() != '\n');
     fgets(student.name, sizeof(student.name), stdin);
     printf("\nNhap ma so sinh vien: ");
-    scanf("%ld", &student.id);
+    scanf("%lld", &student.id);
     printf("\nNhap ngay thang nam sinh: ");
     while(getchar() != '\n');
-    scanf("%c", &student.dob);
+    scanf("%12s", student.dob);
     printf("\nNhap diem GPA: ");
+    while(getchar() != '\n');
     scanf("%f", &student.gpa);
-    stArr[*numOfStudent] = student;
-    (*numOfStudent)++;
-    printf("\nThem sinh vien thanh cong!");
+    stArr[*numOfStudent-1] = student;
+    printf("\nThem sinh vien thanh cong!\n");
 }
 int main()
 {
-    ST *stArr;
+    ST *stArr = NULL;
+     stArr = (ST*)malloc(0);
     int numOfStudent = 0;
     printf("************CHUONG TRINH QUAN LY SINH VIEN************\n");
     printf("#   1. Them sinh vien.                               #\n");
@@ -39,29 +42,33 @@ int main()
     printf("#   7. Hien thi danh sach sinh vien.                 #\n");
     printf("#   0. Thoat.                                        #\n");
     printf("******************************************************\n");
-    printf("Lua chon cua ban: ");
-    int choose;
-    scanf("%d", &choose);
-    switch(choose){
-        case 1:
-        addStudents(stArr, numOfStudent);
-            break;
-        case 2:
-            break;
-        case 3:
-            break;
-        case 4:
-            break;
-        case 5:
-            break;
-        case 6:
-            break;
-        case 7:
-            break;
-        case 0:
-            return 0;
-        default:
-            printf("Lua chon khong nam trong MENU.");
-            return 0;
+    while(1)
+    {
+        printf("Lua chon cua ban: ");
+        int choose;
+        scanf("%d", &choose);
+        switch(choose){
+            case 1:
+                addStudents(stArr, &numOfStudent);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 0:
+                printf("Ban da thoat chuong trinh!");
+                return 0;
+            default:
+                printf("Lua chon khong nam trong MENU.");
+                return 0;
+        }
     }
 }
