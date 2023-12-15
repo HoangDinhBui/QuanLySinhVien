0c16ad85132391e30c5c516ce2606cf26fd21fc1
Hoang-Mr5 <buidinhhoang1910@gmail.com>
2023-12-15 17:52:10 +0700

    Upload function of update student's information

--- quanLySinhVien.cpp
+++ quanLySinhVien.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <stdbool.h>
 typedef struct Students{
     char name[30];
     long long id;
@@ -29,14 +30,37 @@ void addStudent(ST *stArr, int *numOfStudent)
     printf("\nThem sinh vien thanh cong!\n");
 }
 //Func of print student list
-void printStudent(ST *st, int numOfStudent)
+void printStudent(ST *stArr, int numOfStudent)
 {
     printf("    Ten              MSSV         Ngay sinh         GPA\n");
     for (int i = 0; i < numOfStudent; i++)
     {
-        printf("%s                %lld     %s           %f", st[i].name, st[i].id, st[i].dob, st[i].gpa);
+        printf("%s                %lld     %s           %f", stArr[i].name, stArr[i].id, stArr[i].dob, stArr[i].gpa);
         printf("\n");
     }
+}
+//Func of check ID student in "Func of update student's information
+//bool checkID(ST stArr, long long inputID)
+//{
+//    return stArr.id == inputID;
+//}
+//Func of update student's information
+void updateInfor(ST *stArr, int *numOfStudent, int ordinal)
+{
+    ST student;
+    printf("\nNhap ten sinh vien: ");
+    while(getchar() != '\n');
+    fgets(student.name, sizeof(student.name), stdin);
+    printf("\nNhap ma so sinh vien: ");
+    scanf("%lld", &student.id);
+    printf("\nNhap ngay thang nam sinh: ");
+    while(getchar() != '\n');
+    scanf("%12s", student.dob);
+    printf("\nNhap diem GPA: ");
+    while(getchar() != '\n');
+    scanf("%f", &student.gpa);
+    stArr[ordinal - 1] = student;
+    printf("Thong tin da duoc cap nhat!\n");
 }
 int main()
 {
@@ -63,6 +87,10 @@ int main()
                 addStudent(stArr, &numOfStudent);
                 break;
             case 2:
+                int ordinal;
+                printf("Nhap MSSV can chinh sua: ");
+                scanf("%lld", &ordinal);
+                updateInfor(stArr, &numOfStudent, ordinal);
                 break;
             case 3:
                 break;
